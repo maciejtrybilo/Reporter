283fa7348cb413248a8e4203f4906d8e823cc594
Maciej Trybilo <maciektr@gmail.com>
2016-03-07 18:01:54 +0000

    Initial Commit

---
+++ Reporter/Reporter.h
@@ -0,0 +1,19 @@
+//
+//  Reporter.h
+//  Reporter
+//
+//  Created by Maciej Trybilo on 07/03/2016.
+//  Copyright © 2016 Maciej Trybilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reporter.
+FOUNDATION_EXPORT double ReporterVersionNumber;
+
+//! Project version string for Reporter.
+FOUNDATION_EXPORT const unsigned char ReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reporter/PublicHeader.h>
+
+
